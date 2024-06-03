4a424f0d8a524ade0bc1494f831d0e63edee4247
mithraa222 <mithraa222@gmail.com>
2024-06-03 20:40:19 +0530

    Update filter.pipe.ts

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -6,16 +6,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(movielist: Array<any>,title:string, releaseDate:string){ 
+  transform(movielist: Array<MoviesInfo>,title:string, releaseDate:string){ 
    
     let a = []
     if(title != ''){
-      a = movielist.filter((res: any) => res['title'].toLowerCase().indexOf(title.toLowerCase())>-1);
+      a = movielist.filter((res: MoviesInfo) => res['title'].toLowerCase().indexOf(title.toLowerCase())>-1);
     }else{
       a = movielist;
     }
     if(releaseDate != ''){
-      a = a.filter((res: any) => res['release_date'].toLowerCase().indexOf(releaseDate.toLowerCase())>-1);
+      a = a.filter((res: MoviesInfo) => res['release_date'].toLowerCase().indexOf(releaseDate.toLowerCase())>-1);
     }
     return a;
   }
